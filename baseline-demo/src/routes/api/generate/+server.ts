c27a8b3fa53f1e4ae6778c0c65040d9b4c20ac66
Technickel <github@technickel.dev>
2025-10-06 03:04:49 -0400

    Try adding js

--- baseline-demo/src/routes/api/generate/+server.ts
+++ baseline-demo/src/routes/api/generate/+server.ts
@@ -3,12 +3,12 @@ import { GoogleGenAI, mcpToTool } from "@google/genai";
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
 
-export async function POST({ request }) {
-  const transport = new StdioClientTransport({
-    command: "npx",
-    args: ["-y", "@technickel/baseline-mcp"],
-  });
+const transport = new StdioClientTransport({
+  command: "npx",
+  args: ["-y", "@technickel/baseline-mcp"],
+});
 
+export async function POST({ request }) {
   const client = new Client({
     name: "example-client",
     version: "1.0.0",
@@ -20,25 +20,25 @@ export async function POST({ request }) {
   await client.connect(transport);
 
   let text = "";
-  // try {
-  //   const content = await ai.models.generateContent({
-  //     model: "gemini-2.5-flash",
-  //     contents: prompt,
-  //     config: {
-  //       tools: [mcpToTool(client)],
-  //     },
-  //   });
+  try {
+    const content = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+      config: {
+        tools: [mcpToTool(client)],
+      },
+    });
 
-  //   text = content.text || "";
-  // } catch (error) {
-  //   console.log(error);
-  // }
+    text = content.text || "";
+  } catch (error) {
+    console.log(error);
+  }
 
-  // try {
-  //   await client.close();
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    await client.close();
+  } catch (error) {
+    console.log(error);
+  }
 
   return new Response(JSON.stringify({ text: text }), {
     headers: {

d3a774cdc2af3fa53282042356fbd296418f1dfe
Technickel <github@technickel.dev>
2025-10-06 03:27:42 -0400

    Try backslash

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!\usr\bin\env node
 
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
